285b18d0ba214f93ac1cff92324ded54102bdeaa
Konrad Malawski <konrad.malawski@project13.pl>
2011-03-19 15:17:58 +0100

    more initial work, should soon display tweets

--- js/application.js
+++ js/application.js
@@ -1,36 +1,32 @@
-// An example Backbone application contributed by
-// [JĂŠrĂ´me Gravel-Niquet](http://jgn.me/). This demo uses a simple
-// [LocalStorage adapter](backbone-localstorage.html)
-// to persist Backbone models within your browser.
-
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
 
-  // Todo Model
+  // tweet Model
   // -------------------------------------------------------------------------------------------------------------------
 
-  // Our basic **Todo** model has `content`, `order`, and `done` attributes.
-  window.Todo = Backbone.Model.extend({
+  // Our basic **tweet** model has `content`, `order`, and `done` attributes.
+  window.Tweet = Backbone.Model.extend({
 
-    // Default attributes for the todo.
+    // Default attributes for the tweet.
     defaults: {
-      content: "empty todo...",
+      author: "",
+      content: "empty tweet...",
       done: false
     },
 
-    // Ensure that each todo created has `content`.
+    // Ensure that each tweet created has `content`.
     initialize: function() {
       if (!this.get("content")) {
         this.set({"content": this.defaults.content});
       }
     },
 
-    // Toggle the `done` state of this todo item.
+    // Toggle the `done` state of this tweet item.
     toggle: function() {
       this.save({done: !this.get("done")});
     },
 
-    // Remove this Todo from *localStorage* and delete its view.
+    // Remove this tweet from *localStorage* and delete its view.
     clear: function() {
       this.destroy();
       this.view.remove();
@@ -38,51 +34,51 @@ $(function(){
 
   });
 
-  // Todo Collection
+  // Tweet Collection
   // -------------------------------------------------------------------------------------------------------------------
 
-  // The collection of todos is backed by *localStorage* instead of a remote
+  // The collection of tweets is backed by *localStorage* instead of a remote
   // server.
-  window.TodoList = Backbone.Collection.extend({
+  window.TweetList = Backbone.Collection.extend({
 
     // Reference to this collection's model.
-    model: Todo,
+    model: Tweet,
 
-    // Save all of the todo items under the `"todos"` namespace.
-    localStorage: new Store("todos"),
+    // Save all of the tweet items under the `"tweets"` namespace.
+    localStorage: new Store("tweets"),
 
-    // Filter down the list of all todo items that are finished.
+    // Filter down the list of all tweet items that are finished.
     done: function() {
-      return this.filter(function(todo){ return todo.get('done'); });
+      return this.filter(function(tweet){ return tweet.get('done'); });
     },
 
-    // Filter down the list to only todo items that are still not finished.
+    // Filter down the list to only tweet items that are still not finished.
     remaining: function() {
       return this.without.apply(this, this.done());
     },
 
-    // We keep the Todos in sequential order, despite being saved by unordered
+    // We keep the Tweets in sequential order, despite being saved by unordered
     // GUID in the database. This generates the next order number for new items.
     nextOrder: function() {
       if (!this.length) return 1;
       return this.last().get('order') + 1;
     },
 
-    // Todos are sorted by their original insertion order.
-    comparator: function(todo) {
-      return todo.get('order');
+    // Tweets are sorted by their original insertion order.
+    comparator: function(tweet) {
+      return tweet.get('order');
     }
 
   });
 
-  // Create our global collection of **Todos**.
-  window.Todos = new TodoList;
+  // Create our global collection of **Tweets**.
+  window.Tweets = new TweetList;
 
-  // Todo Item View
+  // Tweet Item View
   // -------------------------------------------------------------------------------------------------------------------
 
-  // The DOM element for a todo item...
-  window.TodoView = Backbone.View.extend({
+  // The DOM element for a tweet item...
+  window.TweetView = Backbone.View.extend({
 
     //... is a list tag.
     tagName:  "li",
@@ -93,13 +89,13 @@ $(function(){
     // The DOM events specific to an item.
     events: {
       "click .check"              : "toggleDone",
-      "dblclick div.todo-content" : "edit",
-      "click span.todo-destroy"   : "clear",
-      "keypress .todo-input"      : "updateOnEnter"
+      "dblclick div.tweet-content" : "edit",
+      "click span.tweet-destroy"   : "clear",
+      "keypress .tweet-input"      : "updateOnEnter"
     },
 
-    // The TodoView listens for changes to its model, re-rendering. Since there's
-    // a one-to-one correspondence between a **Todo** and a **TodoView** in this
+    // The TweetView listens for changes to its model, re-rendering. Since there's
+    // a one-to-one correspondence between a **Tweet** and a **TweetView** in this
     // app, we set a direct reference on the model for convenience.
     initialize: function() {
       _.bindAll(this, 'render', 'close');
@@ -107,7 +103,7 @@ $(function(){
       this.model.view = this;
     },
 
-    // Re-render the contents of the todo item.
+    // Re-render the contents of the tweet item.
     render: function() {
       $(this.el).html(this.template(this.model.toJSON()));
       this.setContent();
@@ -115,11 +111,11 @@ $(function(){
     },
 
     // To avoid XSS (not that it would be harmful in this particular app),
-    // we use `jQuery.text` to set the contents of the todo item.
+    // we use `jQuery.text` to set the contents of the tweet item.
     setContent: function() {
       var content = this.model.get('content');
-      this.$('.todo-content').text(content);
-      this.input = this.$('.todo-input');
+      this.$('.tweet-content').text(content);
+      this.input = this.$('.tweet-input');
       this.input.bind('blur', this.close);
       this.input.val(content);
     },
@@ -135,7 +131,7 @@ $(function(){
       this.input.focus();
     },
 
-    // Close the `"editing"` mode, saving changes to the todo.
+    // Close the `"editing"` mode, saving changes to the tweet.
     close: function() {
       this.model.save({content: this.input.val()});
       $(this.el).removeClass("editing");
@@ -166,89 +162,77 @@ $(function(){
 
     // Instead of generating a new element, bind to the existing skeleton of
     // the App already present in the HTML.
-    el: $("#todoapp"),
+    el: $("#tweetsapp"),
 
     // Our template for the line of statistics at the bottom of the app.
     statsTemplate: _.template($('#stats-template').html()),
 
     // Delegated events for creating new items, and clearing completed ones.
     events: {
-      "keypress #new-todo":  "createOnEnter",
-      "keyup #new-todo":     "showTooltip",
-      "click .todo-clear a": "clearCompleted"
+      "fetched tweets": "createOnEnter",
+      "keypress #new-tweet":  "createOnEnter",
+      "click .tweet-clear a": "clearCompleted"
     },
 
-    // At initialization we bind to the relevant events on the `Todos`
+    // At initialization we bind to the relevant events on the `Tweets`
     // collection, when items are added or changed. Kick things off by
-    // loading any preexisting todos that might be saved in *localStorage*.
+    // loading any preexisting tweets that might be saved in *localStorage*.
     initialize: function() {
       _.bindAll(this, 'addOne', 'addAll', 'render');
 
-      this.input    = this.$("#new-todo");
+      this.input    = this.$("#new-tweet");
 
-      Todos.bind('add',     this.addOne);
-      Todos.bind('refresh', this.addAll);
-      Todos.bind('all',     this.render);
+      Tweets.bind('add',     this.addOne);
+      Tweets.bind('refresh', this.addAll);
+      Tweets.bind('all',     this.render);
 
-      Todos.fetch();
+      Tweets.fetch();
     },
 
     // Re-rendering the App just means refreshing the statistics -- the rest
     // of the app doesn't change.
     render: function() {
-      var done = Todos.done().length;
-      this.$('#todo-stats').html(this.statsTemplate({
-        total:      Todos.length,
-        done:       Todos.done().length,
-        remaining:  Todos.remaining().length
+      var done = Tweets.done().length;
+      this.$('#tweet-stats').html(this.statsTemplate({
+        total:      Tweets.length,
+        done:       Tweets.done().length,
+        remaining:  Tweets.remaining().length
       }));
     },
 
-    // Add a single todo item to the list by creating a view for it, and
+    // Add a single tweet item to the list by creating a view for it, and
     // appending its element to the `<ul>`.
-    addOne: function(todo) {
-      var view = new TodoView({model: todo});
-      this.$("#todo-list").append(view.render().el);
+    addOne: function(tweet) {
+      var view = new TweetView({model: tweet});
+      this.$("#tweet-list").append(view.render().el);
     },
 
-    // Add all items in the **Todos** collection at once.
+    // Add all items in the **Tweets** collection at once.
     addAll: function() {
-      Todos.each(this.addOne);
+      Tweets.each(this.addOne);
     },
 
-    // Generate the attributes for a new Todo item.
+    // Generate the attributes for a new Tweet item.
     newAttributes: function() {
       return {
         content: this.input.val(),
-        order:   Todos.nextOrder(),
+        order:   Tweets.nextOrder(),
         done:    false
       };
     },
 
-    // If you hit return in the main input field, create new **Todo** model,
+    // If you hit return in the main input field, create new **Tweet** model,
     // persisting it to *localStorage*.
     createOnEnter: function(e) {
       if (e.keyCode != 13) return;
-      Todos.create(this.newAttributes());
+      Tweets.create(this.newAttributes());
       this.input.val('');
     },
 
-    // Clear all done todo items, destroying their models.
+    // Clear all done tweet items, destroying their models.
     clearCompleted: function() {
-      _.each(Todos.done(), function(todo){ todo.clear(); });
+      _.each(Tweets.done(), function(tweet){ tweet.clear(); });
       return false;
-    },
-
-    // Lazily show the tooltip that tells you to press `enter` to save
-    // a new todo item, after one second.
-    showTooltip: function(e) {
-      var tooltip = this.$(".ui-tooltip-top");
-      var val = this.input.val();
-      tooltip.fadeOut();
-      if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
-      if (val == '' || val == this.input.attr('placeholder')) return;
-      var show = function(){ tooltip.show().fadeIn(); };
-      this.tooltipTimeout = _.delay(show, 1000);
     }
 
   });
