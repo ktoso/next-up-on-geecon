2aa61bb81c1b6cb0b026c885ceca4911bc5e5e57
Konrad Malawski <konrad.malawski@project13.pl>
2013-05-12 02:59:03 +0200

    latest code and jar

--- update-agenda.sh
+++ update-agenda.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-java -cp geecon-gen-agenda-json-0.1.jar pl.project13.meetupgetnames.service.GeeCONSlurper > data/agenda.json
+java -cp geecon-gen-agenda-json-0.1.jar pl.project13.meetupgetnames.Agenda2Json > data/agenda.json
 
-git add data
-git commit -m "Updated agenda"
-git push origin gh-pages
+git add data && git commit -m "Updated agenda" && git push origin gh-pages
